8cbffb1d24a1ac8a2d4f49efb9de281229d18010
Mike <mkoval3@gatech.edu>
2017-11-21 03:07:31 -0500

    page loads correctly now

--- server.js
+++ server.js
@@ -5,35 +5,15 @@ app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-	res.send('hello world');
-})
 
-app.post('/', function(req, res) {
-	console.log('Got a POST request for the homepage');
-	res.send('Hello POST');
-})
 
-app.delete('/del_user', function(req, res) {
-	console.log('Got a DELETE request for the /list_user');
-	res.send('Page Listing');
-})
-
-// This responds a GET request for the /list_user page.
-app.get('/list_user', function (req, res) {
-   console.log('Got a GET request for /list_user');
-   res.send('Page Listing');
-})
-
-// This responds a GET request for abcd, abxcd, ab123cd, and so on
-app.get('/ab*cd', function(req, res) {   
-   console.log('Got a GET request for /ab*cd');
-   res.send('Page Pattern Match');
-})
 
 var server = app.listen(8081, function() {
 	var host = server.address().address
 	var port = server.address().port
 
 	console.log("Example app listening at http://%s:%s", host, port)
+})
+app.get('/', function (req, res) {
+	res.render('main.ejs', {})
 })
