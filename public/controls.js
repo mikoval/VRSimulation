5c27c417f4c7b195d31b054d9faacf7069ab8422
Mike <mkoval3@gatech.edu>
2017-11-22 01:48:51 -0500

    added window resize

--- public/controls.js
+++ public/controls.js
@@ -69,48 +69,56 @@ THREE.PointerLockControls = function ( camera ) {
 };
 
 var onKeyDown = function ( event ) {
-					switch ( event.keyCode ) {
-						case 38: // up
-						case 87: // w
-							moveForward = true;
-							break;
-						case 37: // left
-						case 65: // a
-							moveLeft = true; break;
-						case 40: // down
-						case 83: // s
-							moveBackward = true;
-							break;
-						case 39: // right
-						case 68: // d
-							moveRight = true;
-							break;
-						case 32: // space
-							if ( canJump === true ) velocity.y += 350;
-							canJump = false;
-							break;
-					}
-				};
-				var onKeyUp = function ( event ) {
-					switch( event.keyCode ) {
-						case 38: // up
-						case 87: // w
-							moveForward = false;
-							break;
-						case 37: // left
-						case 65: // a
-							moveLeft = false;
-							break;
-						case 40: // down
-						case 83: // s
-							moveBackward = false;
-							break;
-						case 39: // right
-						case 68: // d
-							moveRight = false;
-							break;
-					}
-				};
-				document.addEventListener( 'keydown', onKeyDown, false );
-				document.addEventListener( 'keyup', onKeyUp, false );
+switch ( event.keyCode ) {
+	case 38: // up
+	case 87: // w
+		moveForward = true;
+		break;
+	case 37: // left
+	case 65: // a
+		moveLeft = true; break;
+	case 40: // down
+	case 83: // s
+		moveBackward = true;
+		break;
+	case 39: // right
+	case 68: // d
+		moveRight = true;
+		break;
+	case 32: // space
+		if ( canJump === true ) velocity.y += 350;
+		canJump = false;
+		break;
+}
+};
+var onKeyUp = function ( event ) {
+switch( event.keyCode ) {
+	case 38: // up
+	case 87: // w
+		moveForward = false;
+		break;
+	case 37: // left
+	case 65: // a
+		moveLeft = false;
+		break;
+	case 40: // down
+	case 83: // s
+		moveBackward = false;
+		break;
+	case 39: // right
+	case 68: // d
+		moveRight = false;
+		break;
+}
+};
+document.addEventListener( 'keydown', onKeyDown, false );
+document.addEventListener( 'keyup', onKeyUp, false );
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
 
